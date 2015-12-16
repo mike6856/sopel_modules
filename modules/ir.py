6c40123c5ac31b4e3f4c1445adea7d2aa9562479
Michael Porras <michael.porras@rackspace.com>
2015-12-16 13:22:04 -0600

    white space

--- modules/ir.py
+++ modules/ir.py
@@ -5,28 +5,43 @@ from sets import Set
 def make_request(tail):
     url = 'http://192.168.1.38:5000/ir/%s' % tail
     r = requests.post(url)
-    print r
-    print r.text
-    return
+    if 'okay' in r.text:
+        return
+    bot.say('IR Server is not happy')
 
 @commands('tv')
 def tv_power(bot, trigger):
     target = trigger.group(2).split(' ')[0]
-    t = target.lower()
+    t = target.lower().strip()
     toggles = Set(['pwr', 'on', 'off', 'toggle'])
+    channels = Set(['fox', 'nbc', 'cbs', 'bot', 'chrome'])
     if t in toggles:
         bot.say('Toggling TV power')
         make_request('tv/on')
+    elif t in channels:
+        bot.say('Changing TV to %s' % t)
+        make_request('tv?channel=%s' % t)
+    elif 'input' in t:
+        bot.say('Next input')
+        make_request('tv/input')
+    else:
+        bot.say('wut?')
     return
 
 @commands('sound')
 def soundbar_power(bot, trigger):
     target = trigger.group(2).split(' ')[0]
-    t = target.lower()
+    t = target.lower().strip()
     toggles = Set(['pwr', 'on', 'off', 'toggle'])
+    channel = Set(['optical', 'bot'])
     if t in toggles:
         bot.say('Toggling soundbar power')
         make_request('soundbar/on')
+    elif t in channel:
+        bot.say('Changing soundbar to %s' % t)
+        make_request('soundbar?button=%s' % t)
+    else:
+        bot.say('wut?')
     return
 
 @commands('vol')
@@ -34,7 +49,7 @@ def volume_commands(bot, trigger):
     target = trigger.group(2).split(' ')[0]
     t = target.lower()
     if 'up' in t or '+' in t :
-        bot.say('Lowering volume')
+        bot.say('Raising volume')
         make_request('soundbar?button=vol_up')
     elif 'down' in t or '-' in t:
         bot.say('Lowering volume')
